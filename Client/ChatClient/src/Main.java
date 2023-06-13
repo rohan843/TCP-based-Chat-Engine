ac1ba7e7ac636ee91f9708979048b8e34e47f080
rohan843 <rohan.sharma2934@gmail.com>
2023-06-13 19:52:38 +0530

    changes

--- Client/ChatClient/src/Main.java
+++ Client/ChatClient/src/Main.java
@@ -1,25 +1,76 @@
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class Main {
-    public static void main(String[] args) {
+class Logger {
+    public enum LogLevels {
+        Info,
+        Warning,
+        Error
+    }
+
+    public static void log(String message, String sender, LogLevels level) {
+        String colorCode = "";
+        String resetColorCode = "\u001B[0m";
+        switch (level) {
+            case Info:
+                colorCode = "\u001B[32m";
+                break;
+
+            case Warning:
+                colorCode = "\u001B[33m";
+                break;
+
+            case Error:
+                colorCode = "\u001B[31m";
+                break;
+
+            default:
+
+                break;
+        }
+        System.out.println(colorCode + sender + ": " + message + resetColorCode);
+    }
+}
+
+class LivelinessAnalyserThread extends Thread {
+    private AtomicReference<Boolean> isConnnectionToServerAlive;
+
+    public LivelinessAnalyserThread(AtomicReference<Boolean> isConnnectionToServerAlive) {
+        this.isConnnectionToServerAlive = isConnnectionToServerAlive;
+    }
+
+    public void run() {
+
         String host = "localhost";
         int port = 10002;
 
         try {
-            // Create a socket connection to the server
             Socket socket = new Socket(host, port);
             System.out.println("Connected to server: " + host + " on port: " + port);
-
-            // Perform any communication or data exchange with the server
-            // ...
-
-            // Close the socket connection
             socket.close();
             System.out.println("Connection closed.");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        while (!isConnnectionToServerAlive.get()) {
+            try {
+                Logger.log("Connection not established... retrying in 10s", "LivelinessAnalyserThread",
+                        Logger.LogLevels.Info);
+                Thread.sleep(10 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
+public class Main {
+    public static AtomicReference<Boolean> isConnnectionToServerAlive = new AtomicReference<Boolean>(false);
+
+    public static void main(String[] args) {
+        
+    }
+}
