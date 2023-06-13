ce767d5f7aeac316d264fc756d7132cee8f60246
rohan843 <rohan.sharma2934@gmail.com>
2023-06-13 19:24:34 +0530

    minor changes

--- Server/ChatServer/src/KeepAliveServer.java
+++ Server/ChatServer/src/KeepAliveServer.java
@@ -9,21 +9,28 @@ public class KeepAliveServer {
             ServerSocket serverSocket = new ServerSocket(10002);
             System.out.println("Server listening on port 10002...");
 
-            while (true) {
-                // Accept incoming connections
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Connected to client: " + clientSocket.getInetAddress());
+            try {
+                while (true) {
+                    // Accept incoming connections
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Connected to client: " + clientSocket.getInetAddress());
 
-                // Set keep-alive option for the socket
-                clientSocket.setKeepAlive(true);
+                    // Set keep-alive option for the socket
+                    clientSocket.setKeepAlive(true);
 
-                // Handle the connection (you can add your custom logic here)
-                // ...
+                    // Handle the connection (you can add your custom logic here)
+                    // ...
 
-                // Close the client socket
-                clientSocket.close();
-                System.out.println("Connection closed");
+                    // Close the client socket
+                    clientSocket.close();
+                    System.out.println("Connection closed");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            
+            serverSocket.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
