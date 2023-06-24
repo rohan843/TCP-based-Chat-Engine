74ff2fb785bc7252a16fbeb43cacdfb2687f948e
rohan843 <rohan.sharma2934@gmail.com>
2023-06-24 16:20:38 +0530

    finish update function

--- Client/ChatClient/src/datastrs/MessagesStore.java
+++ Client/ChatClient/src/datastrs/MessagesStore.java
@@ -1,13 +1,17 @@
 package datastrs;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.Collections;
 import utils.ChatMessage;
 import utils.Message;
 
 public class MessagesStore {
-    // Username --> ChatMessage linked list (sorted on the basis of timestamp, earliest first)
-    ConcurrentHashMap<String, ConcurrentLinkedDeque<ChatMessage>> map = new ConcurrentHashMap<String, ConcurrentLinkedDeque<ChatMessage>>();
+    // Username --> ChatMessage linked list (sorted on the basis of timestamp,
+    // earliest first)
+    private ConcurrentHashMap<String, SortedMap<String, ChatMessage>> map = new ConcurrentHashMap<String, SortedMap<String, ChatMessage>>();
+
     // Update the message store with new values.
     public void update(Message message) {
         String senderUsername = message.getSenderUsername();
@@ -16,6 +20,19 @@ public class MessagesStore {
         String timestamp = message.getMessageTimestamp();
         boolean wasMessageSentToCurrentClient = !message.isOutboundMessage();
 
-        
+        ChatMessage chatMessage = new ChatMessage(senderUsername, receiverUsername, timestamp,
+                wasMessageSentToCurrentClient, messageContent);
+
+        // The person with whom the current user is chatting
+        String conversationUsername = senderUsername;
+        if (!wasMessageSentToCurrentClient) {
+            conversationUsername = receiverUsername;
+        }
+
+        if (!map.containsKey(conversationUsername)) {
+            map.put(conversationUsername, Collections.synchronizedSortedMap(new TreeMap<String, ChatMessage>()));
+        }
+
+        map.get(conversationUsername).put(timestamp, chatMessage);
     }
 }
